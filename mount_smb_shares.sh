99eb21ac96292c8142c05f220ebeabd89ed0a9cd
utylee <utylee@gmail.com>
2025-06-19 23:18:16 +0900

    chg: supervisor.plist autorun chgd to bash -c and sleep 60

---
+++ mount_smb_shares.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Delay to allow network to initialize
+sleep 15
+
+# Mount points and credentials (replace 'user' and 'password' accordingly)
+mkdir -p /Users/utylee/Volumes/4001
+mount_smbfs //utylee:sksmsqnwk11@192.168.1.202/4001 /Users/utylee/Volumes/4001
+
+mkdir -p /Users/utylee/Volumes/8001
+mount_smbfs //utylee:sksmsqnwk11@192.168.1.202/8001 /Users/utylee/Volumes/8001
+
+mkdir -p /Users/utylee/Volumes/4003
+mount_smbfs //utylee:sksmsqnwk11@192.168.1.204/4003 /Users/utylee/Volumes/4003
+
+mkdir -p /Users/utylee/Volumes/5003
+mount_smbfs //utylee:sksmsqnwk11@192.168.1.204/5003 /Users/utylee/Volumes/5003
