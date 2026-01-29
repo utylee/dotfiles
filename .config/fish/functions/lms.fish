7513edc4993ebf1b2983169ecd7510c3f674a8e2
utylee <utylee@gmail.com>
2026-01-29 22:50:08 +0900

    chg: fix lms.fish complete and function

---
+++ .config/fish/functions/lms.fish
@@ -0,0 +1,29 @@
+function lms --description "LM Studio CLI wrapper (cross-platform)"
+    # stop → unload 별칭 유지
+    set -l argv2 $argv
+    if test (count $argv2) -ge 1; and test "$argv2[1]" = "stop"
+        set argv2[1] "unload"
+    end
+
+    switch (uname)
+        case Linux
+            # WSL / Linux
+            if test -n "$WSL_DISTRO_NAME"
+                # WSL: Windows lms 호출
+                set -l args ""
+                for a in $argv2
+                    set a (string replace -a '"' '\"' -- $a)
+                    set args "$args \"$a\""
+                end
+                powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass \
+                    -Command "Set-Location C:\; & lms$args"
+            else
+                # 순수 Linux (lms가 PATH에 있으면)
+                command lms $argv2
+            end
+
+        case Darwin
+            # macOS
+            command lms $argv2
+    end
+end
