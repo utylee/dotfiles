cee00cd860256071fb145d89196d5606447b25c6
utylee <utylee@gmail.com>
2026-02-05 01:39:18 +0900

    chg: samba share by hc

--- bin/mount_smb_shares.sh
+++ bin/mount_smb_shares.sh
@@ -3,6 +3,9 @@
 # Delay to allow network to initialize
 sleep 15
 
+# mount_smbfs //utylee:sksmsqnwk11@192.168.100.108/_share_mac2 /Users/utylee/Volumes/_share_mac2
+mount_smbfs //utylee:sksmsqnwk11@192.168.1.202/_share_mac2 /Users/utylee/Volumes/_share_mac2
+
 # Mount points and credentials (replace 'user' and 'password' accordingly)
 # mkdir -p /Users/utylee/Volumes/4001
 mount_smbfs //utylee:sksmsqnwk11@192.168.1.202/4001 /Users/utylee/Volumes/4001
